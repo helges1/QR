15bff37f3f0bd3711d516433996c4ca9daa95e4b
helges1 <sebhelg@gmail.com>
2020-04-05 18:37:55 +0200

    Add files via upload

---
+++ script/qr-url.js
@@ -0,0 +1,55 @@
+
+
+
+
+
+	var qrcode = new QRCode(document.getElementById("qrcode"), {
+		width : 250,
+        height : 250,
+
+        
+       
+        
+	
+    });
+    
+  
+
+	function makeCode () {		
+        var elText = document.getElementById("text");
+
+ 
+        qrcode.makeCode(elText.value);
+        
+
+    }
+    
+    	$("#text").
+		on("blur", function () {
+			makeCode();
+		}).
+		on("keydown", function (e) {
+			if (e.keyCode == 13) {
+				makeCode();
+			}
+		});
+
+	
+    makeCode()
+
+	
+
+
+
+    $(document).ready(function () {
+        $("#text").change(function() {
+            if (!/:\/\//.test(this.value)) {
+                this.value = "http://" + this.value;
+                makeCode () 
+            }
+            
+        });
+    });
+
+
+
